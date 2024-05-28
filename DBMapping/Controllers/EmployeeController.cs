1858ea9c30cd6e5955f24d497891a54e0707edc1
moazam-zintech <muhammad.moazam@zintechnologies.com>
2024-05-28 14:15:36 +0500

    Retrieve  data from Data base trough Eager Loading

--- DBMapping/Controllers/EmployeeController.cs
+++ DBMapping/Controllers/EmployeeController.cs
@@ -1,10 +1,7 @@
 ﻿using DBMapping.Data;
 using DBMapping.Model;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.EntityFrameworkCore;
-
 namespace DBMapping.Controllers
 {
     [Route("api/[controller]")]
@@ -13,51 +10,53 @@ namespace DBMapping.Controllers
     {
         private readonly IConfiguration _config;
         private readonly ApplicationDBContext _dbContext;
-        public EmployeeController(IConfiguration configuration,ApplicationDBContext dBContext) {
+        public EmployeeController(IConfiguration configuration, ApplicationDBContext dBContext)
+        {
             _dbContext = dBContext;
-        _config = configuration;
+            _config = configuration;
         }
-
         [HttpPost]
-
         public async Task<IActionResult> AddEmployee(EmployeeRequest request)
         {
             var employee = new Employee
             {
                 Name = request.Name,
                 Id = Guid.NewGuid(),
-            };
-
+        };
             var address = new Address
             {
                 AddressId = employee.Id,
-                City= request.City,
+                City = request.City,
                 State = request.State,
             };
-
             var department = new Department
             {
-                DepartmentName=request.DepartmentName,
-                DaprtmentId=employee.Id,
-                DepartmentLoc=request.DepartmentLoc,
-
+                DepartmentName = request.DepartmentName,
+                DaprtmentId = employee.Id,
+                DepartmentLoc = request.DepartmentLoc,
             };
-
             employee.Address = address;
             employee.Department = department;
-           await _dbContext.Employee.AddAsync(employee);
-           await _dbContext.SaveChangesAsync();
-            
+            await _dbContext.Employee.AddAsync(employee);
+            await _dbContext.SaveChangesAsync();
             return Ok(employee);
         }
+        [HttpGet]
+        public ActionResult GetEmployee()
+        {
 
+            //Eager Loading 
+           var response= _dbContext.Employee.Include(s=>
+               s.Address).Include(s=>s.Department);
+            return Ok(response);    
+        }
         public class EmployeeRequest
         {
             public String Name { get; set; }
             public string City { get; set; }
             public string State { get; set; }
             public string DepartmentName { get; set; }
-            public string DepartmentLoc { get; set;}
+            public string DepartmentLoc { get; set; }
         }
     }
 }
